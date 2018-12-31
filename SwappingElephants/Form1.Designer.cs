1e9c00ba0bd7ae66ca23746dffd2c51ba5a10c38
Tavia Sterling <TaviaSterling@outlook.com>
2018-12-31 13:38:24 -0700

    elephant class

--- SwappingElephants/Form1.Designer.cs
+++ SwappingElephants/Form1.Designer.cs
@@ -28,46 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.lloydButton = new System.Windows.Forms.Button();
+            this.lucindaButton = new System.Windows.Forms.Button();
+            this.swapButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // lloydButton
             // 
-            this.button1.Location = new System.Drawing.Point(100, 55);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.lloydButton.Location = new System.Drawing.Point(100, 55);
+            this.lloydButton.Name = "lloydButton";
+            this.lloydButton.Size = new System.Drawing.Size(75, 32);
+            this.lloydButton.TabIndex = 0;
+            this.lloydButton.Text = "Lloyd";
+            this.lloydButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // lucindaButton
             // 
-            this.button2.Location = new System.Drawing.Point(100, 104);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.lucindaButton.Location = new System.Drawing.Point(100, 104);
+            this.lucindaButton.Name = "lucindaButton";
+            this.lucindaButton.Size = new System.Drawing.Size(75, 35);
+            this.lucindaButton.TabIndex = 1;
+            this.lucindaButton.Text = "Lucinda";
+            this.lucindaButton.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // swapButton
             // 
-            this.button3.Location = new System.Drawing.Point(100, 161);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.swapButton.Location = new System.Drawing.Point(100, 161);
+            this.swapButton.Name = "swapButton";
+            this.swapButton.Size = new System.Drawing.Size(75, 35);
+            this.swapButton.TabIndex = 2;
+            this.swapButton.Text = "Swap!";
+            this.swapButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.swapButton);
+            this.Controls.Add(this.lucindaButton);
+            this.Controls.Add(this.lloydButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -76,9 +76,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button lloydButton;
+        private System.Windows.Forms.Button lucindaButton;
+        private System.Windows.Forms.Button swapButton;
     }
 }
 
